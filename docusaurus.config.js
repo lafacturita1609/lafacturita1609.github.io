4c2e1bbea48355319cb0e9af83ee779a3ea69e33
Deyvis <soporte@buho.la>
2023-08-15 22:29:59 -0500

    Fix minor Update CSS 3.0

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -126,7 +126,7 @@ const config = {
         // copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
       },
       prism: {
-        theme: lightCodeTheme,
+        theme: darkCodeTheme,
         darkTheme: darkCodeTheme,
       },
       theme: {
