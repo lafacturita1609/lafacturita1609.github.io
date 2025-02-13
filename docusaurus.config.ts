0dd0c0879bdaf4dfdbc60bf894fc666fa3b3b2b2
Cristian Vega <critianvega6150@gmail.com>
2025-02-13 17:18:39 -0500

    Cambio de url hacia manual.uio.la

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -12,10 +12,10 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://documentacion.fastura.app',
+  url: 'https://manual.uio.la',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/documentacion/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -25,6 +25,7 @@ const config: Config = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
+  trailingSlash: false,
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
