8ad35cc5a9ea15f47c555a3d2b4b30bd5e428512
Jean-ZR <jzapatareyes12@gmail.com>
2025-02-27 09:37:27 -0500

    cambiar img

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -182,7 +182,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/logo_buho.png',
       navbar: {
         title: 'Fastura',
         logo: {
