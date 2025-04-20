745ec28cac20eb268b952862eae8e352df87c2a1
Enrique ZR <zrenrique033@gmail.com>
2025-04-20 09:15:17 -0500

    subiendo la repo de pruebas

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -12,14 +12,14 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://manual.uio.la',
+  url: 'https://Jean-ZR.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: 'documentacion',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'fastura',
+  organizationName: 'Jean-ZR', // Usually your GitHub org/user name.
   projectName: 'documentacion',
 
   onBrokenLinks: 'throw',
