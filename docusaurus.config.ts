8d1d139e6300d266f91a2cd727f99a58e1a2813a
Enrique ZR <zrenrique033@gmail.com>
2025-04-20 14:09:03 -0500

    subiendo al repositorio principal, cambios realizados en la seccion del api de QR BUHO, y cambio en el nombre de la carpeta Reseller por Muti Empresa

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -12,14 +12,14 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://Jean-ZR.github.io/',
+  url: 'https://manual.uio.la/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Jean-ZR', // Usually your GitHub org/user name.
+  organizationName: 'fastura', // Usually your GitHub org/user name.
   projectName: 'documentacion',
 
   onBrokenLinks: 'throw',
